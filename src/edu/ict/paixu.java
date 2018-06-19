f5c9b75cf62f1d6c0b2893721c2323cb3afc062e
guyaojiang <1483260200@qq.com>
2018-06-19 20:45:22 +0800

    添加后续的工程 Signed-off-by: guyaojiang <1483260200@qq.com>

---
+++ src/edu/ict/paixu.java
@@ -0,0 +1,33 @@
+package 冒泡排序;
+import java.util.Arrays;
+/**
+ * 冒泡排序改进版
+ * @author mmz
+ *
+ */
+public class BubbleSort1 {
+    public static void BubbleSort(int[] arr) {
+        boolean flag = true;
+        while(flag){
+            int temp;//定义一个临时变量
+            for(int i=0;i<arr.length-1;i++){//冒泡趟数，n-1趟
+                for(int j=0;j<arr.length-i-1;j++){
+                    if(arr[j+1]<arr[j]){
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                        flag = true;
+                    }
+                }
+                if(!flag){
+                    break;//若果没有发生交换，则退出循环
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[]{1,6,2,2,5};
+        BubbleSort.BubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
